ddd849cb31801766d4dab4ab4a142a785e328faa
Karen Lu <karen_lu123@hotmail.com>
2016-01-20 20:27:46 -0500

    Create c# folder and upload ExtractFromGovSite

---
+++ c#/ExtractFromGovSite/ExtractFromGovSite/Extraction.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ExtractFromGovSite
+{
+    partial class Extraction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.extractBtn = new System.Windows.Forms.Button();
+            this.progressBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // extractBtn
+            // 
+            this.extractBtn.Location = new System.Drawing.Point(35, 22);
+            this.extractBtn.Name = "extractBtn";
+            this.extractBtn.Size = new System.Drawing.Size(204, 37);
+            this.extractBtn.TabIndex = 0;
+            this.extractBtn.Text = "Start";
+            this.extractBtn.UseVisualStyleBackColor = true;
+            this.extractBtn.Click += new System.EventHandler(this.extractBtn_Click);
+            // 
+            // progressBox
+            // 
+            this.progressBox.Location = new System.Drawing.Point(34, 91);
+            this.progressBox.Multiline = true;
+            this.progressBox.Name = "progressBox";
+            this.progressBox.Size = new System.Drawing.Size(204, 135);
+            this.progressBox.TabIndex = 1;
+            // 
+            // Extraction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.progressBox);
+            this.Controls.Add(this.extractBtn);
+            this.Name = "Extraction";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button extractBtn;
+        private System.Windows.Forms.TextBox progressBox;
+    }
+}
+
---
+++ c#/ExtractFromGovSite/ExtractFromGovSite/Extraction.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtractFromGovSite
+{
+    public partial class Extraction : Form
+    {
+        public Extraction()
+        {
+            InitializeComponent();
+        }
+
+        private void extractBtn_Click(object sender, EventArgs e)
+        {
+            progressBox.Text = "Retrieving Data and Writing to file...";
+            //for (int id = 2; id <= 7356; id++)
+                getData(225);
+            progressBox.Text = "Finished.";
+        }
+
+        private void getData(int id)
+        {
+            List<string> names = new List<string>();
+            List<string> titles = new List<string>();
+            List<string> phones = new List<string>();
+            WebClient web = new WebClient();
+            String html = web.DownloadString("http://www.infogo.gov.on.ca/infogo/office.do?actionType=telephonedirectory&infoType=telephone&unitId=" + id + "&locale=en");
+            MatchCollection m1 = Regex.Matches(html, "<A class=\"employee\" href=javascript:browseEmployee\\('.+?'\\)>\\s*(.+?)\\s*</A>", RegexOptions.Singleline);
+            MatchCollection m2 = Regex.Matches(html, "<A class=\"employee\".+?</A>\\]\\s*\\[\\s*(.+?)\\s*\\]", RegexOptions.Singleline);
+            MatchCollection m3 = Regex.Matches(html, "<A class=\"employee\".+?</A>\\]\\s*\\[.+?\\].+?\\[(.+?)\\]", RegexOptions.Singleline);
+            string dept = Regex.Match(html, "<a  href=.+?actionType=telephonedirectory&infoType=telephone&unitId=.+?>\\s*(.+?)\\s*</a>").Groups[1].Value;
+            string city = Regex.Match(html, "<font class='bodycontext'>(.+?)&nbsp;").Groups[1].Value;
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\klu\\Desktop\\depts.txt", true))
+            {
+                foreach (Match m in m1)
+                {
+                    sw.WriteLine(dept);
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\klu\\Desktop\\cities.txt", true))
+            {
+                foreach (Match m in m1)
+                {
+                    sw.WriteLine(city);
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\klu\\Desktop\\names.txt", true))
+            {
+                foreach (Match m in m1)
+                {
+                    string name = m.Groups[1].Value;
+                    names.Add(name);
+                    sw.WriteLine(name);
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\klu\\Desktop\\titles.txt", true))
+            {
+                foreach (Match m in m2)
+                {
+                    string title = m.Groups[1].Value;
+                    titles.Add(title);
+                    sw.WriteLine(title);
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\klu\\Desktop\\phones.txt", true))
+            {
+                foreach (Match m in m3)
+                {
+                    string phone = m.Groups[1].Value;
+                    phones.Add(phone);
+                    sw.WriteLine(phone);
+                }
+            }
+
+        }
+    }
+}
